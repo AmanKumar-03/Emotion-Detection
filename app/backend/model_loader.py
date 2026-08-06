85381049043577a91d87ce5461632b68154dc936
AmanKumar-03 <amankumar.ds032gmail.com>
2026-08-06 20:01:25 +0530

    add docker ci cd workflow

--- app/backend/model_loader.py
+++ app/backend/model_loader.py
@@ -3,51 +3,45 @@ import pickle
 import logging
 
 
+# Logger configuration
 logger = logging.getLogger("model_loader")
 logger.setLevel(logging.INFO)
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(
-        os.path.dirname(
-            os.path.abspath(__file__)
-        )
-    )
-)
+# Current backend directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MODEL_PATH = os.path.join(
-    BASE_DIR,
-    "artifacts",
-    "model.pkl"
-)
+# Artifacts folder path Docker container path: /app/artifacts
+ARTIFACTS_DIR = os.path.join(BASE_DIR,"artifacts")
 
-VECTORIZER_PATH = os.path.join(
-    BASE_DIR,
-    "artifacts",
-    "vectorizer.pkl"
-)
+# Model files
+MODEL_PATH = os.path.join(ARTIFACTS_DIR,"model.pkl")
+VECTORIZER_PATH = os.path.join(ARTIFACTS_DIR,"vectorizer.pkl")
+LABEL_ENCODER_PATH = os.path.join(ARTIFACTS_DIR,"label_encoder.pkl")
 
-LABEL_ENCODER_PATH = os.path.join(
-    BASE_DIR,
-    "artifacts",
-    "label_encoder.pkl"
-)
+def load_pickle(path, name):
+    """Load pickle file safely"""
+    try:
+        with open(path, "rb") as file:
+            obj = pickle.load(file)
+        logger.info(f"{name} loaded successfully")
+        return obj
 
-# Load Model
-with open(MODEL_PATH, "rb") as file:
-    model = pickle.load(file)
-logger.info("Model loaded successfully")
+    except FileNotFoundError:
+        logger.error(f"{name} file not found: {path}")
+        raise
+    except Exception as e:
+        logger.error(f"Error loading {name}: {e}")
+        raise
 
-# Load Vectorizer
-with open(VECTORIZER_PATH, "rb") as file:
-    vectorizer = pickle.load(file)
-logger.info("Vectorizer loaded successfully")
+# Load Trained ML Model
+model = load_pickle(MODEL_PATH,"Model")
+
+# Load TF-IDF Vectorizer
+vectorizer = load_pickle(VECTORIZER_PATH,"Vectorizer")
 
 # Load Label Encoder
 label_encoder = None
-
 if os.path.exists(LABEL_ENCODER_PATH):
-    with open(LABEL_ENCODER_PATH,"rb") as file:
-        label_encoder = pickle.load(file)
-    logger.info("Label encoder loaded")
+    label_encoder = load_pickle(LABEL_ENCODER_PATH,"Label Encoder")
 else:
-    logger.warning("Label encoder not found")
+    logger.warning("Label encoder not found. Skipping...")
