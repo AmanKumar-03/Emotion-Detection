993a56accab3ad8f124eebe06a0aa81ca122a50e
AmanKumar-03 <amankumar.ds032gmail.com>
2026-08-07 16:50:50 +0530

    Use self-hosted runner

--- app/backend/model_loader.py
+++ app/backend/model_loader.py
@@ -3,45 +3,80 @@ import pickle
 import logging
 
 
-# Logger configuration
 logger = logging.getLogger("model_loader")
 logger.setLevel(logging.INFO)
 
-# Current backend directory
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Artifacts folder path Docker container path: /app/artifacts
-ARTIFACTS_DIR = os.path.join(BASE_DIR,"artifacts")
 
-# Model files
-MODEL_PATH = os.path.join(ARTIFACTS_DIR,"model.pkl")
-VECTORIZER_PATH = os.path.join(ARTIFACTS_DIR,"vectorizer.pkl")
-LABEL_ENCODER_PATH = os.path.join(ARTIFACTS_DIR,"label_encoder.pkl")
+ARTIFACTS_DIR = os.path.join(
+    BASE_DIR,
+    "artifacts"
+)
+
+
+MODEL_PATH = os.path.join(
+    ARTIFACTS_DIR,
+    "model.pkl"
+)
+
+VECTORIZER_PATH = os.path.join(
+    ARTIFACTS_DIR,
+    "vectorizer.pkl"
+)
+
+LABEL_ENCODER_PATH = os.path.join(
+    ARTIFACTS_DIR,
+    "label_encoder.pkl"
+)
+
+
 
 def load_pickle(path, name):
-    """Load pickle file safely"""
+
     try:
         with open(path, "rb") as file:
             obj = pickle.load(file)
+
         logger.info(f"{name} loaded successfully")
+
         return obj
 
     except FileNotFoundError:
-        logger.error(f"{name} file not found: {path}")
-        raise
-    except Exception as e:
-        logger.error(f"Error loading {name}: {e}")
+        logger.error(f"{name} not found: {path}")
         raise
 
-# Load Trained ML Model
-model = load_pickle(MODEL_PATH,"Model")
 
-# Load TF-IDF Vectorizer
-vectorizer = load_pickle(VECTORIZER_PATH,"Vectorizer")
+
+# Load Model
+model = load_pickle(
+    MODEL_PATH,
+    "Model"
+)
+
+
+# Load Vectorizer
+vectorizer = load_pickle(
+    VECTORIZER_PATH,
+    "Vectorizer"
+)
+
 
 # Load Label Encoder
+
 label_encoder = None
+
+
 if os.path.exists(LABEL_ENCODER_PATH):
-    label_encoder = load_pickle(LABEL_ENCODER_PATH,"Label Encoder")
+
+    label_encoder = load_pickle(
+        LABEL_ENCODER_PATH,
+        "Label Encoder"
+    )
+
 else:
-    logger.warning("Label encoder not found. Skipping...")
+
+    logger.warning(
+        "Label encoder not found. Skipping..."
+    )
